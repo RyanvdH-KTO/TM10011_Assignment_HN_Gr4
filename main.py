e74ecf4e41e4f1a543b2d32885e15c5b5e33b818
RyanvdH-KTO <ryan.vd.hoek@outlook.com>
2026-02-24 14:21:10 +0100

    dataframe

--- main.py
+++ main.py
@@ -4,9 +4,11 @@
 #%% Load Data
 # Load Data
 from hn.load_data import load_data
+import pandas as pd
 data = load_data()
 print(f'The number of samples: {len(data.index)}')
-
 print(f'The number of columns: {len(data.columns)}')
+
+data = pd.DataFrame(data)
 
 # %%
