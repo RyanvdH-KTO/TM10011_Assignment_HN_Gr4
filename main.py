762c9b599ecc9a5f208a5b8331abf4b89ca93718
Ewout <ewout.van.riet@hotmail.com>
2026-03-03 14:35:32 +0100

    cleaning

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-#%% Load Data
-# Load Data
-from hn.load_data import load_data
+#%% packages
+# load packages
 import pandas as pd
 
+# load functions
+from hn.load_data import load_data
+
+#%% Data
 
 data = load_data()
 print(f'The number of samples: {len(data.index)}')
