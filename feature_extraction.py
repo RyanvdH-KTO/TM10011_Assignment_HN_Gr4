c32b514df280085db3c1d446df190584db8f6982
“Katherine <kglasz@tudelft.nl>
2026-03-17 13:53:38 +0100

    feature selection probeersels

--- feature_extraction.py
+++ feature_extraction.py
@@ -6,6 +6,11 @@ import matplotlib.pyplot as plt
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_selection import SequentialFeatureSelector
 
 #%% Import and select data
 #Import and select data
@@ -71,3 +76,88 @@ for group in groups:
 
 
 # %%
+# Univariate feature selection: kiest de beste k features op basis van hun individuele relatie met de target
+def select_k_best_anova(X_train, X_test, y_train, k=10): #k zegt hoeveel features je wil
+    selector = SelectKBest(score_func=f_classif, k=k) #SelectKBest: kijkt naar alle features afzonderlijk, geeft elke feature een score, en kiest daarna de k hoogste scores
+                                                      # f_classif: betekent dat je de ANOVA F-test gebruikt
+    X_train_sel = selector.fit_transform(X_train, y_train)
+    X_test_sel = selector.transform(X_test)
+
+    selected_indices = selector.get_support(indices=True) #welke features gekozen zijn
+    scores = selector.scores_ #ANOVA scores per feature
+
+    print("Selected feature indices:", selected_indices)
+    print("Scores of all features:", scores)
+    print("Shape train after selection:", X_train_sel.shape)
+    print("Shape test after selection:", X_test_sel.shape)
+
+    return X_train_sel, X_test_sel, selected_indices, scores, selector
+
+
+from preprocessing import preprocess_pipeline
+X_train, X_test, y_train, y_test, scaler = preprocess_pipeline(scale_method="standard")
+
+X_train_sel, X_test_sel, selected_indices, scores, selector = select_k_best_anova(
+    X_train, X_test, y_train, k=10
+)
+
+
+
+
+# %%
+# RFE: recursive feature elimination
+def rfe_selection(X_train, X_test, y_train, estimator, n_features=10):
+
+    selector = RFE(estimator=estimator, n_features_to_select=n_features)
+
+    X_train_sel = selector.fit_transform(X_train, y_train)
+    X_test_sel = selector.transform(X_test)
+
+    selected_indices = selector.get_support(indices=True)
+    ranking = selector.ranking_
+
+    print("Selected feature indices:", selected_indices)
+    print("Ranking of all features:", ranking) #Ranking zegt iets over in welke ronde de feature eruit is gegooid
+    print("Shape train after selection:", X_train_sel.shape)
+    print("Shape test after selection:", X_test_sel.shape)
+
+    return X_train_sel, X_test_sel, selected_indices, ranking, selector
+
+
+# RFE testen met Logistic Regression
+X_train_rfe, X_test_rfe, selected_idx, ranking, selector = rfe_selection(
+    X_train,
+    X_test,
+    y_train,
+    estimator=LogisticRegression(max_iter=1000, random_state=42),
+    n_features=10
+)
+
+
+
+# %%
+# Sequential Feature Selector
+from sklearn.feature_selection import SequentialFeatureSelector
+def sfs_selection(X_train, X_test, y_train, estimator, n_features=10, direction="forward", scoring="accuracy", cv=5):
+    selector = SequentialFeatureSelector(
+        estimator,
+        n_features_to_select=n_features,
+        direction=direction,
+        scoring=scoring,
+        cv=cv,
+        n_jobs=-1
+    )
+
+    X_train_sel = selector.fit_transform(X_train, y_train)
+    X_test_sel = selector.transform(X_test)
+
+    selected_indices = selector.get_support(indices=True)
+
+    print("Selected feature indices:", selected_indices)
+    print("Shape train after selection:", X_train_sel.shape)
+    print("Shape test after selection:", X_test_sel.shape)
+
+    return X_train_sel, X_test_sel, selected_indices, selector
+
+# SFS testen met Logistic Regression
+
