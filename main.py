1ad99fee25c0c2fd29eae8146f296cd46093296d
RyanvdH-KTO <ryan.vd.hoek@outlook.com>
2026-03-24 14:12:12 +0100

    update iterations

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ def main():
     grid_search_pls_da = GridSearchCV(pipeline_pls_da, param_grid_pls_da, 
                                     cv=kf, scoring=["accuracy", "roc_auc", "f1"], refit = 'roc_auc', n_jobs=-1)
     grid_search_pls_da.fit(X_train_filtered, y_train)
-
+    print(grid_search_pls_da)
     pls_da_model = grid_search_pls_da.best_estimator_ 
     y_pred_pls_da = pls_da_model.predict(X_validate_filtered)
     probabilities_pls_da = pls_da_model.predict_proba(X_validate_filtered)
@@ -152,7 +152,7 @@ def main():
     # Pipeline Support Vector Machine
     pipeline_SVM = Pipeline(steps=[
     ('classifier', SVC(random_state=42, 
-                     max_iter=5000, 
+                     max_iter=1000, 
                      class_weight='balanced', 
                      kernel = 'linear',
                      shrinking = True,
@@ -224,125 +224,6 @@ def main():
     print(f"CL Report of XGB:", classification_report(y_validate, y_pred_XGB, zero_division='warn'))
     plot_auc(y_validate, propabilities_XGB[:,1], "XGBoost model")
     confussion_matrix(y_validate, y_pred_XGB, "XGBoost model")
-
-    # Pipeline Random Forest
-    pipeline_forest = Pipeline(steps=[
-        ('classifier', RandomForestClassifier(class_weight='balanced', random_state=42)) 
-    ])
-    
-    param_dist_forest = {"classifier__n_estimators": [10, 15, 20, 30, 100],
-                "classifier__max_depth":[8,15,25],
-                "classifier__min_samples_leaf":[1,2,5,10,15,100],
-                "classifier__max_leaf_nodes": [2, 5, 10]}
-
-    grid_search_forest = GridSearchCV(pipeline_forest, param_dist_forest, cv=kf, scoring=["accuracy", "roc_auc", "f1"], 
-    refit = "roc_auc", n_jobs=-1)
-    
-    grid_search_forest.fit(X_train_filtered, y_train) 
-    classifier_forest = grid_search_forest.best_estimator_ 
-    y_pred_forest = classifier_forest.predict(X_validate_filtered)  
-    propabilities_forest = classifier_forest.predict_proba(X_validate_filtered)[:, 1]
-    propabilities_forest = np.column_stack([1-propabilities_forest, propabilities_forest])
-
-    print('Best parameters found:\n', grid_search_forest.best_params_)
-    print("Beste score:", grid_search_forest.best_score_)
-    print(f"CL Report of RF:", classification_report(y_validate, y_pred_XGB, zero_division='warn'))
-    plot_auc(y_validate, propabilities_forest[:,1], "Random forest model")
-    confussion_matrix(y_validate, y_pred_forest, "Random forest model")
-
-#%%
-
-    #Pipeline that compares feature selectors and classifiers
-    feature_selectors = {
-        "k best ANOVA" : SelectKBest(score_func=f_classif, k=10),
-        "RFE"          : RFE(estimator=LogisticRegression(max_iter=1000), n_features_to_select=5),
-    }
-
-    # Feature selection
-    # Deze uitvoeren voor estimator = logistic regresion en voor de SVM. 
-
-    # Logistic Regression estimator for feature selection
-    lr_estimator = LogisticRegression(
-        penalty='l2',
-        solver='liblinear',
-        class_weight='balanced',
-        random_state=42,
-        max_iter=10000
-    )
-    # Deze gebruikte ik even om te testen, maar hier moet dan goeie LR model of SVM
-
-    # Feature selection
-    # Select k best ANOVA
-    X_train_anova, X_test_anova = select_k_best_anova(
-    X_train_filtered,
-    X_validate_filtered,
-    y_train,
-    k=10
-    )
-
-    # print("Shape train after k best ANOVA:", X_train_anova.shape)
-    # print("Shape test after ANOVA:", X_test_anova.shape)
-
-
-    # SFS forward
-    X_train_sfs_fwd, X_test_sfs_fwd = sfs_selection(
-    X_train_filtered,
-    X_validate_filtered,
-    y_train,
-    estimator=lr_estimator,
-    direction="forward",
-    scoring="accuracy", #kan ook roc-auc
-    cv=5
-    ) 
-
-    # print("Shape train after SFS forward:", X_train_sfs_fwd.shape)
-    # print("Shape test after SFS forward:", X_test_sfs_fwd.shape)
-
-    # SFS backward
-    X_train_sfs_bwd, X_test_sfs_bwd = sfs_selection(
-        X_train_filtered,
-        X_validate_filtered,
-        y_train,
-        estimator=lr_estimator,
-        direction="backward",
-        scoring="accuracy",
-        cv=5
-    )  
-
-    # print("Shape train after SFS backward:", X_train_sfs_bwd.shape)
-    # print("Shape test after SFS backward:", X_test_sfs_bwd.shape)
-
-    # RFE
-    X_train_rfe, X_test_rfe = rfe_selection(
-        X_train_filtered,
-        X_validate_filtered,
-        y_train,
-        estimator=lr_estimator,
-        n_features=10
-    )
-
-    # print("Shape train after RFE:", X_train_rfe.shape)
-    # print("Shape test after RFE:", X_test_rfe.shape)
-
-    # Logistic Regression Classifier
-    '''Needs feature selection beforehand, so we compare different methods to be able to use the best in the pipeline'''
-
-    #%% SVM classifier
-    #SVM classifier
-    '''Needs feature selection beforehand, so we compare different methods to be able to use the best in the pipeline'''
-
-
-    #%% XGBoost Classifier
-    #XGBoost Classifier
-    '''Doesn't need further feature selection, since this method handles that itself'''
-
-    #%% Partial Least Square classifer
-    #PLS classifier
-    '''Doesn't need further feature selection, since this method handles that itself'''
-
-
-    #%% Classifier Evaluation 
-
 
 #%% Run model
 # Run model
